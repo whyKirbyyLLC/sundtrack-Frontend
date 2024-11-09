cd87b18997f872a0236f200fd69130c8b8481785
whyKirbyy <timschmid02@icloud.com>
2024-11-09 16:35:51 +0100

    db conneciton is there

---
+++ app/lib/db.ts
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cachedClient: MongoClient | null = null;
+
+export async function getMongoClient() {
+    if (cachedClient) {
+        return cachedClient;
+    }
+    try {
+        if (!MONGODB_URI) {
+            throw new Error("MONGODB_URI is not defined in the environment variables.");
+        }
+        const client = await MongoClient.connect(MONGODB_URI as string);
+        cachedClient = client;
+        return client;
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+        throw error;
+    }
+}
+
+export async function closeConnection() {
+    if (cachedClient) {
+        await cachedClient.close();
+        cachedClient = null;
+    }
+} 
--- app/posthog_client.ts
+++ app/posthog_client.ts
@@ -6,9 +6,10 @@ export function initPostHog() {
     if (typeof window !== 'undefined') {
         posthog.init('phc_75WOmtugBO95jJ6H7m6X0plzYM0mDIgIGqF6ZCqjog2',
             {
-                api_host: 'https://eu.i.posthog.com',
-                person_profiles: 'always'
-            }
-        )
+                api_host: "https://eu.i.posthog.com",
+                person_profiles: "identified_only",
+                autocapture: true,
+                capture_pageview: true,
+            });
     }
 }
