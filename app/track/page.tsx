3d0621dfc67fc01884235622787c3515777abf5d
whyKirbyy <timschmid02@icloud.com>
2024-11-13 21:18:30 +0100

    some more stuff

--- app/track/page.tsx
+++ app/track/page.tsx
@@ -3,7 +3,15 @@ import styles from "./page.module.css";
 export default function Track() {
     return (
         <div className={styles.page}>
-            <h1>Track</h1>
+            <div className={styles.titleContainer}>
+                <p className={styles.title}>daily diet diary</p>
+                <p className={styles.subtitle}>
+                    For the best results, try tracking your meals for at least a week to get the most accurate insights and achieve the best results.
+                </p>
+            </div>
+            <div className={styles.formContainer}>
+                Hi
+            </div>
         </div>
     );
 }
